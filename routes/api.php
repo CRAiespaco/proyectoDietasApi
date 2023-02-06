8eb98135f5a9c9e139710cafe415b9a53886a44a
CRAiespaco <christopherrodriguea.alu@iespacomolla.es>
2023-02-06 16:27:40 +0100

    06/02/2023

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 });*/
 
 
-Route::get('/categoria',[CategoriaController::class,'indexx']);
+Route::get('/categoria',[CategoriaController::class,'index']);
 
 Route::get('/', function () {
     return view('welcome');

16f365954f82c8193624279a746cdc6aa7abc297
Niko017 <nikolaydiaz.alu@iespacomolla.es>
2023-02-12 16:52:23 +0100

    user

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,13 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    protected $fillable = ['nombre','correo','contrasenya',];
+    protected $fillable = ['nombre','email','contrasenya',];
+
+    protected $hidden = ['contrasenya','remember_token'];
 
     public function recetas(){
         return $this->belongsToMany(Receta::class);
--- database/migrations/2023_01_31_143213_create_usuarios_table.php
+++ database/migrations/2023_01_31_143213_create_usuarios_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->string('nombre');
-            $table->string('correo');
+            $table->string('email')->unique();
+            $table->timestamp('email_verificado')->nullable();
             $table->string('contrasenya');
+            $table->rememberToken();
             $table->timestamps();
         });
     }

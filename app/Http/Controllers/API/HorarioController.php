88dd73ebaf552ba2e4f7a68cc5b602f4955499e1
CRAiespaco <christopherrodriguea.alu@iespacomolla.es>
2023-03-11 19:03:36 +0100

    controllers

--- app/Http/Controllers/API/HorarioController.php
+++ app/Http/Controllers/API/HorarioController.php
@@ -2,7 +2,96 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\Horario;
+
 class HorarioController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response(Horario::all());
+    }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+
+        $validacion = Validator::make($request->all(),[
+            "fecha"=>"Date",
+        ]);
+        if($validacion->fails()){
+            return response("La Horario no ha podido ser almacenada",Response::HTTP_BAD_REQUEST);
+        }else{
+            $horario = new Horario();
+            $horario->horaInicio=$request['horaInicio'];
+            $horario->save();
+
+            $respuesta = [
+                "mensaje"=>'Horario creado correctamente',
+                'Horario'=>$horario
+            ];
+
+            return response()->json($respuesta);
+        }
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Horario $horario)
+    {
+        return response(Horario::with('receta')->find($horario->id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $horario)
+    {
+        $validacion=Validator::make((array)$request,[
+            "fecha"=>"Date",
+        ]);
+
+        if($validacion->fails()){
+            return response("La receta no se pudo modificar");
+        }else{
+            $horario = new Horario();
+            $horario->horaInicio=$request['horaInicio'];
+            $horario->save();
+
+            return response()->json($horario);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
+     */
+    public function destroy(Horario $horario)
+    {
+        Horario::destroy($horario);
+        return response()->json([
+            "mensaje"=>"Se ha borrado correctamente",
+            "Horario"=>$horario
+        ],Response::HTTP_BAD_REQUEST);
+    }
 }
